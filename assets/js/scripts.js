98d14ab9a044c91ce132e5dac05b1d98ff801c5c
RookGit <rookcorp@yandex.ru>
2019-03-11 12:17:45 +0400

    Install all plugins final! :)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,4 +2,12 @@
  * Created by RookDeveloper on 11.03.2019.
  */
 
-alert("123");
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+
+    sayName() {
+        console.log(`Person ${this.name} said his name`);
+    }
+}
